c0d0c493036a4e3371dca1a9134748c90ff4d392
sabbir <bsse1014@iit.du.ac.bd>
2022-01-24 00:28:40 +0600

    added search proerty

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import article_detail
+from .views import *
 
 urlpatterns = [
-    path('<int:id>/', article_detail, name="article_detail")
+    path('<int:id>/', article_detail, name="article_detail"),
+    path('',article_search)
 ]
--- articles/views.py
+++ articles/views.py
@@ -8,3 +8,18 @@ def article_detail(request, id= None):
         article_object = Article.objects.get(id=id)
 
     return render(request, 'article/details.html', {'article' : article_object})
+
+
+def article_search(request):
+    query_dict = request.GET
+
+    try:
+        query = int(query_dict.get('q'))
+    except:
+        query = None
+
+    query_obj = None
+    if query is not None:
+        query_obj = Article.objects.get(id=query)
+
+    return render(request, 'article/search.html',{'article':query_obj})
