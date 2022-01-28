fdb87144455df3d7be5b5da4bc324f5baa9379ad
sabbir <bsse1014@iit.du.ac.bd>
2022-01-28 20:40:26 +0600

    updated field of article

--- articles/admin.py
+++ articles/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from .models import Article
 
 class ArticleAdmin(admin.ModelAdmin):
-    list_display = ['id' , 'title']
+    list_display = ['id' , 'title','create_time','update','publish']
     search_fields = ['title', 'content']
 
 admin.site.register(Article, ArticleAdmin)
--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+import datetime
 
 class Article(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=200)
     content = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    publish = models.DateField(auto_now_add=False, null=True, blank=True)
